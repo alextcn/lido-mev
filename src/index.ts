14df1990804732b2397da7423895b819f3182a23
Alex Tkachenko <skyalexx@gmail.com>
2023-05-14 14:45:04 +0100

    multiple builders

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,16 @@ import 'dotenv/config'
 const CHAIN_ID = 1
 const provider = new providers.InfuraProvider(CHAIN_ID, process.env.RPC_URL)
 
-// const FLASHBOTS_ENDPOINT = 'https://relay-goerli.flashbots.net'
-const FLASHBOTS_ENDPOINT = 'https://relay.flashbots.net'
+// const BUILDER_ENDPOINTS_GOERLI = ['https://relay-goerli.flashbots.net']
+
+// list of builders for sending bundle to increase a chance
+const BUILDER_ENDPOINTS = [
+  'https://builder0x69.io', // buildHash null error
+  // 'https://rpc.beaverbuild.org', // SERVER_ERROR response, 403 Forbidden
+  // 'https://relay.flashbots.net',
+  // 'https://rsync-builder.xyz' // buildHash null error,
+  // 'https://rpc.titanbuilder.xyz'
+]
 
 if (process.env.WALLET_PRIVATE_KEY === undefined) {
   console.error('Please provide WALLET_PRIVATE_KEY env')
@@ -22,13 +30,14 @@ if (process.env.AUTH_SIGNER_KEY === undefined) {
 async function main() {
   const wallet = new Wallet(process.env.WALLET_PRIVATE_KEY, provider)
   const authSigner = new Wallet(process.env.AUTH_SIGNER_KEY)
-  const flashbotsProvider = await FlashbotsBundleProvider.create(
-    provider,
-    authSigner,
-    FLASHBOTS_ENDPOINT
-  )
-
   console.log(`wallet: ${wallet.address}, signer: ${authSigner.address}`)
+
+  // TODO: make any provider connection can fail safe
+  const flashbotsProviders = await Promise.all(
+    BUILDER_ENDPOINTS.map((endpoint) =>
+      FlashbotsBundleProvider.create(provider, authSigner, endpoint)
+    )
+  )
 
   const block = await provider.getBlockNumber()
   const bundleBlock = block + 5
@@ -41,40 +50,52 @@ async function main() {
   // )
   // console.log(`bundle stats: ${JSON.stringify(res)}`)
 
-  send bundle
-  const bundleSubmitResponse = await flashbotsProvider.sendBundle(
-    [
-      {
-        // transaction: {
-        //   chainId: CHAIN_ID,
-        //   type: 2,
-        //   value: ethers.utils.parseEther('0.03'),
-        //   to: '0x20EE855E43A7af19E407E39E5110c2C1Ee41F64D',
-        //   data: '0x1249c58b', // calldata for mint()
-        //   maxFeePerGas: ethers.utils.parseUnits('4', 'gwei'),
-        //   maxPriorityFeePerGas: ethers.utils.parseUnits('3', 'gwei')
-        // },
-        transaction: {
-          chainId: CHAIN_ID,
-          type: 2,
-          value: ethers.utils.parseEther('0.00000024'),
-          to: '0xaAaaa0cf858828c5dA523acdeAD993AB13007EA7',
-          maxFeePerGas: ethers.utils.parseUnits('100', 'gwei'),
-          maxPriorityFeePerGas: ethers.utils.parseUnits('12', 'gwei')
-        },
-        signer: wallet
+  // prep and send bundle
+  const bundleTransactions = [
+    {
+      transaction: {
+        chainId: CHAIN_ID,
+        type: 2,
+        value: ethers.utils.parseEther('0.00000024'),
+        to: '0xaAaaa0cf858828c5dA523acdeAD993AB13007EA7',
+        maxFeePerGas: ethers.utils.parseUnits('100', 'gwei'),
+        maxPriorityFeePerGas: ethers.utils.parseUnits('12', 'gwei')
+      },
+      signer: wallet
+    }
+  ]
+
+  // run bundle for all providers async
+  let submittions = 0
+  await Promise.all(
+    flashbotsProviders.map(async (provider) => {
+      const key = provider.connection.url
+      const bundleResponse = await provider.sendBundle(bundleTransactions, bundleBlock)
+
+      if ('error' in bundleResponse) {
+        console.warn(`[${key}]`, bundleResponse.error.message)
+        return
       }
-    ],
-    bundleBlock
+
+      // simulate not supported by many builders
+      // console.log(`[${key}]`, await bundleResponse.simulate())
+      console.log(`[${key}]`, bundleResponse)
+
+      submittions++
+    })
   )
+  console.log(`bundle sent to ${submittions}/${flashbotsProviders.length} providers`)
 
-  if ('error' in bundleSubmitResponse) {
-    console.warn(bundleSubmitResponse.error.message)
-    return
-  }
-
-  console.log(await bundleSubmitResponse.simulate())
-  console.log(await bundleSubmitResponse.receipts())
+  // const bundleSubmitResponse = await flashbotsProviders[0].sendBundle(
+  //   bundleTransactions,
+  //   bundleBlock
+  // )
+  // if ('error' in bundleSubmitResponse) {
+  //   console.warn(bundleSubmitResponse.error.message)
+  //   return
+  // }
+  // console.log(await bundleSubmitResponse.simulate())
+  // console.log(await bundleSubmitResponse.receipts())
 }
 
 main()
